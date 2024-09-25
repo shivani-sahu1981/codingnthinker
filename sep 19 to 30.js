e5931caaf9e95183828aabd39092a8eb5f4b63e2
shivani-sahu1981 <shivanisahu1981@gmail.com>
2024-09-25 21:22:39 +0530

    first commit

---
+++ sep 19 to 30.js
@@ -0,0 +1,354 @@
+/*let h2= document.queryselector('h2')
+
+for (let i of h2){
+    i.style.color="red"
+}*/
+
+/*let div= document.querySelector('div')
+
+  let h2= document.createElement('h2')
+
+  h2.innerText='hello'
+
+  div.appendChild(h2)
+  console.log(div,"dikh jaa.....");*/
+
+ /* let a= document.querySelector('a')
+
+  console.log(a.getAttribute('href'));
+  a.setAttribute('href','http://flipkart.com')*/
+
+  /*const arr = [
+    'https://images.unsplash.com/photo-1726587912121-ea21fcc57ff8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3fHx8ZW58MHx8fHx8',
+    'https://images.unsplash.com/photo-1692533436686-5c04084a8296?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw5fHx8ZW58MHx8fHx8',
+    'https://plus.unsplash.com/premium_photo-1698406096055-91a364147db5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyOHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1726486896376-4d1340e2f672?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyN3x8fGVufDB8fHx8fA%3D%3D'
+  ]
+  const imageEl = document.querySelector('img');
+
+  let num = 0;
+
+  setInterval(function(){
+    imageEl.setAttribute('src', arr[num]);
+    num = (num + 1) % arr.length;
+    num++
+  },1000)*/
+
+ /* function fun1(){
+    console.log('hiii');
+  }*/
+
+  // let btn= document.querySelector('button')
+
+  /*btn.onclick=function(){
+    console.log('hello');
+  }*/
+ 
+  // btn.addEventListener('click',function(){
+  //   console.log('hello');
+  // }) 
+
+  // 21 sep 2024
+
+
+ /* let btn= document.querySelector('button')
+  let body= document.querySelector('body')
+  let h2= document.querySelector('h2')
+  btn.addEventListener('click',function(){
+       console.log('hello');
+       h2.innerText='hello'
+       body.style.backgroundColor='green'
+  })*/
+
+ /* let inp= document.querySelector('input')
+
+  inp.addEventListener('input',(e)=>{
+    // console.log('hiiiiii');
+     console.log(e.target.value);
+  })*/
+
+  /*   let formE= document.querySelector('form')
+
+     formE.addEventListener('submit',(e)=>{
+      e.preventDefault()
+      console.log('hello')
+      console.log(formE.elements[0].value);
+      console.log(formE.elements[1].value);
+     })*/
+
+
+      /*let inp= document.querySelector('input')
+      let btn= document.querySelector('button')
+      let ul= document.querySelector('ul')
+
+      btn.addEventListener('click',()=>{
+        let data=inp.value
+
+        let li= document.createElement('li')
+        li.innerText=data
+
+        ul.append(li)
+        inp.value=''
+
+      })*/
+
+
+       /* let inp= document.querySelector('input')
+        let button= document.querySelectorAll('button')
+        for(let btn of button){
+          btn.addEventListener('click',(e)=>{
+            let text= e.target.innerText
+            if(text==='c'){
+              inp.value=''
+            }
+            else if(text==='='){
+              inp.value= eval(inp.value)
+            }
+            else{
+              inp.value=inp.value+text
+            }
+          })
+        }*/
+
+
+        // <h1>24 septemer 2024</h1>
+console.log('heheheh');
+
+      /*  let canvas= document.querySelector('canvas')
+        let pen= canvas.getContext('2d')
+  
+        let cell=50
+        let snakeC= [[0,0]]
+        let direction= 'right'
+        let gameover
+        let id= setInterval(()=>{
+          draw()
+          update()
+        },200)
+
+        document.addEventListener('keydown',(e)=>{
+          if(e.key==='ArrowLeft'){
+            direction='left'
+          }
+          else if(e.key==='ArrowDown'){
+            direction='down'
+          }
+          else if(e.key==='ArrowUp'){
+            direction='up'
+          }
+          else{
+            direction='right'
+          }
+        })
+
+        function draw(){
+          pen.clearRect(0,0, 1200,600)
+          for(let i of snakeC){
+            pen.fillStyle='red'
+            pen.fillRect(i[0],i[1],cell,cell)
+          }
+        }
+        function update(){
+          let headx= snakeC[snakeC.length-1][0]
+          let heady= snakeC[snakeC.length-1][1]
+          let newx 
+          let newy
+          if(direction==='up'){
+            newx=headx
+            newy=heady-cell
+          }
+          else if(direction==='down'){
+            newx=headx
+            newy=heady+cell
+          }
+          else if(direction==='left'){
+            newx=headx-cell
+            newy=heady
+          }
+          else{
+            newx=headx+cell
+            newy=heady
+          }
+          snakeC.push([newx,newy])
+          snakeC.shift()
+        }*/
+
+          
+
+
+
+
+// 1199,599
+  
+
+ let canvas=  document.querySelector('canvas')
+ let pen=   canvas.getContext('2d')
+ pen.fillStyle='red'
+ let cell=50
+ let snakeC=[[0,0]]
+ let direction='right'
+ let score=100
+ let gameOver=false
+ let randomData=randomCell()
+  let id=  setInterval(()=>{
+    draw()
+    update()
+
+ },200)
+
+//  pen.fillRect(0,0,cell,50)
+
+
+document.addEventListener('keydown',(e)=>{
+    // console.log(e,"he");
+    if(e.key==='ArrowLeft'){
+        direction='left'
+    }
+    else if(e.key==='ArrowDown'){
+        direction='down'
+    }
+    else if(e.key==='ArrowUp'){
+        direction='up'
+    }
+    else{
+        direction='right'
+    }
+    
+    // console.log('hehehe');
+    
+
+})
+
+
+function draw(){
+    if(gameOver===true){
+        pen.font='40px sans-sarif'
+        pen.fillStyle='white'
+        pen.fillText('gameOver',200,300)
+        clearInterval(id)
+        return
+    }
+    pen.clearRect(0,0,1200,600)
+    for(let i of snakeC){
+        pen.fillStyle='red'
+        pen.fillRect(i[0],i[1],cell,cell)
+    }
+    pen.fillStyle='green'
+    pen.fillRect(randomData[0],randomData[1],cell,cell  )
+    pen.font='50px sans-sarif'
+    pen.fillText(`${score}`,500,200)
+
+}
+function update(){
+   let headX=  snakeC[snakeC.length-1][0]
+  let headY=  snakeC[snakeC.length-1][1]
+   let newX
+   let newY
+   if(direction==='up'){
+    newX=headX
+    newY=headY-cell
+    if(newY<0){
+        gameOver=true
+    }
+
+   }
+   else if(direction==='down'){
+    newX=headX
+    newY=headY+cell
+    if(newY===600){
+        gameOver=true
+    }
+   }
+   else if(direction==='left'){
+    newX=headX-cell
+    newY=headY
+    if(newX<0){
+        gameOver=true
+    }
+   }
+   else{
+    newX=headX+cell
+    newY=headY
+    if(newX===1200){
+        gameOver=true
+    }
+
+   }
+   if(newX===randomData[0] && newY===randomData[1]){
+     randomData=   randomCell()
+     score+=1
+   }
+   else{
+    snakeC.shift()
+   }
+   snakeC.push([newX,newY])
+
+//   console.log(headX,headY,"hdehehe");
+  
+
+
+}
+
+function randomCell(){
+    return [
+      Math.floor(Math.random()*(1200-cell)/cell)*cell  ,
+        
+        Math.floor(Math.random()*(600-cell)/cell)*cell
+    ]
+}
+// console.log(randomCell());
+
+
+
+
+
+// console.log(Math.random()*1200,"hehe");
+// 
+
+       /*fetch('https://dummyjson.com/user/me').then((res)=>{
+        return res.json()
+       }).then((data)=>{
+        console.log(data);
+       })*/
+
+        /*let apikey='9f6290d6cda9a36a63755fadee71f83d'
+        let city='bhopal'
+        
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`).
+        then((res)=>{
+            return res.json()
+        
+        }).then((data)=>{
+            console.log(data);
+            
+        })*/
+
+        /*let btn= document.querySelector('button')
+        btn.addEventListener('click',()=>{
+          let inp= document.querySelector('input')
+          let city=inp.value
+          let h2= document.querySelector('h2')
+          let apikey='9f6290d6cda9a36a63755fadee71f83d'
+          fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`).
+        then((res)=>{
+            return res.json()
+        
+        }).then((data)=>{
+            console.log(data);
+            h2.innerText=data.main.temp
+        })
+        })*/
+
+        // fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`).
+        // then((res)=>{
+        //     return res.json()
+        
+        // }).then((data)=>{
+        //     console.log(data);
+            
+        // })
+
+        // letbtn= document.querySelector('button')
+
+
+
---
+++ snake24.js
@@ -0,0 +1,256 @@
+
+// let canvas=  document.querySelector('canvas')
+
+// let ctx=    canvas.getContext('2d')
+// let cell=50
+// let Score=10
+// let gameOver=false
+// let direction='right'
+// let scell=[[0,0]]
+// let foodCell=randomCell()
+// let id=  setInterval(()=>{
+//   draw()
+//   update()
+// },100)
+// //   ctx.fillRect(0,0,50,50)
+// document.addEventListener('keydown',function(e){
+//   if(e.key==='ArrowUp'){
+//       direction='up'
+  
+      
+//   }
+//   else if(e.key==='ArrowDown'){
+//       direction='down'
+    
+//   }
+//   else if(e.key==='ArrowLeft'){
+//       direction='left'
+    
+//   }
+//   else{
+//       direction='right'
+    
+//   }
+//   // console.log(e);
+  
+
+// })
+
+// function draw(){
+//   if(gameOver===true){
+//       clearInterval(id)
+//       ctx.font='50px sans-sarif'
+//     ctx.fillText('gameOver',50,100)
+//       return
+
+//   }
+//   ctx.clearRect(0,0,1200,600)
+//   for(let i of scell){
+//       ctx.fillStyle='red'
+//       ctx.fillRect(i[0],i[1],cell,cell)
+//   }
+//     ctx.fillStyle='green'
+//   ctx.fillRect(foodCell[0],foodCell[1],cell,cell)
+//   ctx.font='30px sans-sarif'
+//   ctx.fillText(`${Score}`,300,200)
+
+// }
+// draw()
+// function update(){
+
+// let headX=   scell[scell.length-1][0]
+//    let headY=  scell[scell.length-1][1]
+//  let newX
+// let newY
+// if(direction==='right'){
+//   newX=headX+cell
+//   newY=headY
+//   if(newX===1200){
+//       gameOver=true
+//   }
+
+// }
+// else if(direction==='down'){
+//   newX=headX
+//   newY=headY+cell
+//   if(newY===600){
+//       gameOver=true
+//   }
+// }
+// else if(direction==='left'){
+//   newX=headX-cell
+//   newY=headY
+//   if(newX<0){
+//       gameOver=true
+//   }
+// }
+// else{
+//   newX=headX
+//   newY=headY-cell
+//   if(newY<0){
+//       gameOver=true
+//   }
+// }
+// //    if(newX===1200){
+// //     gameOver=true
+// //    }
+
+
+// if(newX===foodCell[0]  && newY===foodCell[1]){
+// foodCell=  randomCell()
+// Score+=1
+// }
+// else{
+//   scell.shift()
+
+// }
+// scell.push([newX,newY])
+
+
+
+
+// }
+
+
+// function randomCell(){
+//   return [
+//       Math.floor(Math.random()*(1200-cell)/cell)*cell
+//       ,
+//       Math.floor( Math.random()*(600-cell)/cell)*cell
+     
+//   ]
+// }
+
+
+// // console.log(randomCell());
+
+
+
+// 1199,599
+  
+
+let canvas=  document.querySelector('canvas')
+let pen=   canvas.getContext('2d')
+pen.fillStyle='red'
+let cell=50
+let snakeC=[[0,0]]
+let direction='right'
+let score=100
+let gameOver=false
+let randomData=randomCell()
+ let id=  setInterval(()=>{
+   draw()
+   update()
+
+},100)
+
+//  pen.fillRect(0,0,cell,50)
+
+
+document.addEventListener('keydown',(e)=>{
+   // console.log(e,"he");
+   if(e.key==='ArrowLeft'){
+       direction='left'
+   }
+   else if(e.key==='ArrowDown'){
+       direction='down'
+   }
+   else if(e.key==='ArrowUp'){
+       direction='up'
+   }
+   else{
+       direction='right'
+   }
+   
+   // console.log('hehehe');
+   
+
+})
+
+
+function draw(){
+   if(gameOver===true){
+       pen.font='40px sans-sarif'
+       pen.fillStyle='white'
+       pen.fillText('gameOver',200,300)
+       clearInterval(id)
+       return
+   }
+   pen.clearRect(0,0,1200,600)
+   for(let i of snakeC){
+       pen.fillStyle='red'
+       pen.fillRect(i[0],i[1],cell,cell)
+   }
+   pen.fillStyle='green'
+   pen.fillRect(randomData[0],randomData[1],cell,cell  )
+   pen.font='50px sans-sarif'
+   pen.fillText(`${score}`,500,200)
+
+
+
+}
+function update(){
+  let headX=  snakeC[snakeC.length-1][0]
+ let headY=  snakeC[snakeC.length-1][1]
+  let newX
+  let newY
+  if(direction==='up'){
+   newX=headX
+   newY=headY-cell
+   if(newY<0){
+       gameOver=true
+   }
+
+  }
+  else if(direction==='down'){
+   newX=headX
+   newY=headY+cell
+   if(newY===600){
+       gameOver=true
+   }
+  }
+  else if(direction==='left'){
+   newX=headX-cell
+   newY=headY
+   if(newX<0){
+       gameOver=true
+   }
+  }
+  else{
+   newX=headX+cell
+   newY=headY
+   if(newX===1200){
+       gameOver=true
+   }
+
+  }
+  if(newX===randomData[0] && newY===randomData[1]){
+    randomData=   randomCell()
+    score+=1
+  }
+  else{
+   snakeC.shift()
+  }
+  snakeC.push([newX,newY])
+
+//   console.log(headX,headY,"hdehehe");
+ 
+
+
+}
+
+function randomCell(){
+   return [
+     Math.floor(Math.random()*(1200-cell)/cell)*cell  ,
+       
+       Math.floor(Math.random()*(600-cell)/cell)*cell
+   ]
+}
+// console.log(randomCell());
+
+
+
+
+
+// console.log(Math.random()*1200,"hehe");
+// 
