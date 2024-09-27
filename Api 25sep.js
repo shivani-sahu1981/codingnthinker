3d00bf5ee7aa732dc7bc65e5825703ec2d90ca72
shivani-sahu1981 <shivanisahu1981@gmail.com>
2024-09-27 13:01:09 +0530

    api

--- Api 25sep.js
+++ Api 25sep.js
@@ -32,7 +32,8 @@
 // })
 
 // })
-let div= document.querySelector('div')
+
+/*let div= document.querySelector('div')
 
 fetch('https://my-json-server.typicode.com/horizon-code-academy/fake-movies-api/movies').
 then((res)=>{
@@ -57,4 +58,58 @@ then((res)=>{
     // document.createElement('h2')
     
 
-})
+})*/
+
+// fetch('https://api.tvmaze.com/search/shows?q=${searchText}').then((res)=>{
+
+// })
+
+// let btn=   document.querySelector('button')
+// let inp=   document.querySelector('input')
+
+// btn.addEventListener('click',()=>{
+
+/*let btn= document.querySelector('button')
+let inp= document.querySelector('input')
+let div= document.querySelector('div')
+btn.addEventListener('click',()=>{
+    let searchText=inp.value
+
+    fetch(`https://api.tvmaze.com/search/shows?q=${searchText}`).
+    then((res)=>{
+        return res.json()
+    }).then((data)=>{
+        console.log(data[0],'h');*/
+
+       /* let img= document.createElement('img')
+        let h2= document.createElement('h2')
+        h2.innerText=data[0].show.name
+        img.setAttribute('src',data[0].show.image.medium)
+        div.append(img,h2)*/
+       /* data.map((a)=>{
+            console.log(a.show.image.medium,'hehehe');
+            let img= document.createElement('img')
+            let h2= document.createElement('h2')
+            h2.innerText=data[0].show.name
+           img.setAttribute('src',data[0].show.image.medium)
+           div.append(img,h2)
+        })
+    })
+})*/
+
+let arr=[1,2,3,4,5,6,7]
+let [a,b,cs]=arr
+console.log(a);
+
+let obj={
+    id:1,
+    name:"ayushi",
+    age:16
+}
+let {id,age}=obj
+console.log(obj.id,age);
+
+// function sum(a,b,c,d,...num){
+//     console.log();
+// }
+// sum(5,34,5,6,3,6,567)
